b88fa1fd66e350f13e8fc8db5ad9bfc035f2b00d
echoshade800 <echoshade800@gmail.com>
2025-11-13 23:15:39 -0800

    Updated MonsterCard.tsx

--- components/MonsterCard.tsx
+++ components/MonsterCard.tsx
@@ -44,15 +44,16 @@ export function MonsterCard({
         </View>
       </View>
 
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+
       <View style={styles.imageContainer}>
         <Image
           source={{ uri: imageUrl }}
           style={[styles.monsterImage, { width: imageSize, height: imageSize, marginTop: imageOffset }]}
           resizeMode="contain"
         />
-        <View style={styles.descriptionContainer}>
-          <Text style={styles.description}>{description}</Text>
-        </View>
       </View>
 
       <View style={styles.footer}>
@@ -125,17 +126,12 @@ const styles = StyleSheet.create({
     color: '#000000',
   },
   descriptionContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
     paddingHorizontal: 12,
     paddingVertical: 6,
     borderRadius: 20,
     alignSelf: 'flex-start',
     maxWidth: '98%',
-    marginHorizontal: 4,
   },
   description: {
     fontSize: 12,
@@ -149,7 +145,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical: 8,
-    position: 'relative',
   },
   monsterImage: {
     width: '100%',
