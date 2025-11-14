2c469aea4c4d2cb2fdaa28c23ca2c89695cc04e7
fanthus <fanthus@163.com>
2025-11-14 20:46:38 +0800

    tmp

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,16 +5,22 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 =======
 // @ts-ignore - expo-splash-screen types are included in the package
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+<<<<<<< HEAD
 >>>>>>> 63ebd74 (增加启动闪屏)
+=======
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+>>>>>>> 5bb3131 (tmp)
 
 export default function Layout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="camera" options={{ headerShown: false }} />
-        <Stack.Screen name="photo-text" options={{ headerShown: false }} />
-      </Stack>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="camera" options={{ headerShown: false }} />
+          <Stack.Screen name="photo-text" options={{ headerShown: false }} />
+        </Stack>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
