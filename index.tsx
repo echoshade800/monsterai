8db98042c2ddb13b5f0939d4791f194e2633e0dd
fanthus <fanthus@163.com>
2025-11-17 23:36:05 +0800

    commit bundle

---
+++ index.tsx
@@ -0,0 +1,12 @@
+// import { AppRegistry } from 'react-native';
+// import App from './app/(tabs)/index'; // 确保路径正确指向你的根组件
+
+// AppRegistry.registerComponent('StepsMiniApp', () => App);
+
+import { ExpoRoot } from 'expo-router';
+
+// 让 expo-router 扫描 ./app 目录
+function Root() {
+  const ctx = (require as any).context ? (require as any).context('./app') : undefined;
+  return <ExpoRoot context={ctx} />;
+}
