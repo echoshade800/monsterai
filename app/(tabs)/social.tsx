9ad592c12bc0257c1099009ada3c53e5b8700515
echoshade800 <echoshade800@gmail.com>
2025-11-16 23:58:51 -0800

    Updated social.tsx

--- app/(tabs)/social.tsx
+++ app/(tabs)/social.tsx
@@ -4,7 +4,7 @@ export default function SocialTab() {
   return (
     <View style={styles.container}>
       <Image
-        source={{ uri: 'https://dzdbhsix5ppsc.cloudfront.net/monster/materials/comingsoon.png' }}
+        source={{ uri: 'https://dzdbhsix5ppsc.cloudfront.net/monster/materials/social2.png' }}
         style={styles.image}
         resizeMode="cover"
       />
