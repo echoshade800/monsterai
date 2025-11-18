9f3b21ead4312cee85072a6f520f941ef70a436b
echoshade800 <echoshade800@gmail.com>
2025-11-18 18:25:19 +0800

    Updated InputField.tsx

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -94,7 +94,7 @@ export function InputField({ onFocus, onSend, isSending = false, disabled = fals
     const expanded = keyboardHeight.value > 0 ? 1 : 0;
 
     return {
-      paddingLeft: interpolate(expanded, [0, 1], [32, 12]),
+      paddingLeft: interpolate(expanded, [0, 1], [26, 12]),
     };
   });
 
@@ -251,7 +251,7 @@ const styles = StyleSheet.create({
   },
   penIconWrapper: {
     position: 'absolute',
-    left: 18,
+    left: 16,
     justifyContent: 'center',
     alignItems: 'center',
     pointerEvents: 'none',
